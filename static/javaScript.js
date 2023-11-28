0e8a9f83c7c10d0fb7da1236e5e08f95d832813d
tariq7299 <tr.sar77an78@gmail.com>
2023-11-29 00:57:40 +0200

    Finished, now doing some adjustments

--- static/javaScript.js
+++ static/javaScript.js
@@ -9,9 +9,10 @@ function copyText(button) {
     var tooltip = button.nextElementSibling;
     tooltip.innerHTML = "Click to Copy";
   }
-
-
+  
   function showSubcategories(category) {
+
+    document.querySelector('.sub-sub-category').style.display = 'none';
     document.querySelector('.sub-category').style.display = 'block';
     var subCatTitle = document.getElementById("subcat-title");
     var row = document.querySelector(".sub-category-row");
@@ -30,31 +31,32 @@ function copyText(button) {
         "Taping BD": ["Taping M/C"],
         "Soldering BD": ["Solder"]
     };
-    var subsubcategories = {
-      "Applicator Problem": ["Scan Applicator ID"], 
-      "Schleuniger": ["Machine Problem"], 
-      "Komax": ["CAO Problem", "CFA Problem", "Crimping Problem", "Gripper Problem", "PC Problem", "Press Problem", "Seal station Problem", "Stripping Problem", "Swivel Problem", "Topwin Problem", "Wire Drive Problem", "Wire length Problem", "Wire Tray Problem"],
-      "Ulmer": ["Ulmer Machine Problem"],
-      "Applicator Change": ["Applicator Change Problem"], 
-      "Crimping Press": ["Crimping Press Problem"], 
-      "DSG Canusa": ["Heaters Problem", "Shrinking Cap Problem", "Shrinking Machine Problem"], 
-      "Heat Gun": ["Heat Gun Problem", "Shrink Problem"], 
-      "Clip Gun": ["Clip Gun Problem"], 
-      "Grommet": ["Grommet Problem"], 
-      "layup": ["layup Problem"], 
-      "Line & Carousel": ["المبة لا تعمل", "أعطال المحرك", "سرعة المحرك غير ثابتة"], 
-      "Pin Punching": ["Pin Punching Problem"], 
-      "Printer": ["Printer Problem"], 
-      "Seal Machine": [" الماكينة لا تعمل", "السيل لا يدخل التراك"], 
-      "Test Module": ["Test Module number is:"], 
-      "Test Table": ["التست لا يعمل", "الكشاف  لا يعمل", "وجود مشكلة فى الاسكان"], 
-      "Twisting M/C": ["Twist Clamp Problem", "Twist Door problem", "Twist Problem"], 
-      "USS M/C": ["Terminal Change", "Welding Machine Problem", "Welding Problem"], 
-      "Taping M/C": ["Tapping problem", "Tapping Touch Screen Problem"], 
-      "Solder": ["Solder Problem"]
-    };
+    // var subsubcategories = {
+    //   "Applicator Problem": ["Scan Applicator ID"], 
+    //   "Schleuniger": ["Machine Problem"], 
+    //   "Komax": ["CAO Problem", "CFA Problem", "Crimping Problem", "Gripper Problem", "PC Problem", "Press Problem", "Seal station Problem", "Stripping Problem", "Swivel Problem", "Topwin Problem", "Wire Drive Problem", "Wire length Problem", "Wire Tray Problem"],
+    //   "Ulmer": ["Ulmer Machine Problem"],
+    //   "Applicator Change": ["Applicator Change Problem"], 
+    //   "Crimping Press": ["Crimping Press Problem"], 
+    //   "DSG Canusa": ["Heaters Problem", "Shrinking Cap Problem", "Shrinking Machine Problem"], 
+    //   "Heat Gun": ["Heat Gun Problem", "Shrink Problem"], 
+    //   "Clip Gun": ["Clip Gun Problem"], 
+    //   "Grommet": ["Grommet Problem"], 
+    //   "layup": ["layup Problem"], 
+    //   "Line & Carousel": ["المبة لا تعمل", "أعطال المحرك", "سرعة المحرك غير ثابتة"], 
+    //   "Pin Punching": ["Pin Punching Problem"], 
+    //   "Printer": ["Printer Problem"], 
+    //   "Seal Machine": [" الماكينة لا تعمل", "السيل لا يدخل التراك"], 
+    //   "Test Module": ["Test Module number is:"], 
+    //   "Test Table": ["التست لا يعمل", "الكشاف  لا يعمل", "وجود مشكلة فى الاسكان"], 
+    //   "Twisting M/C": ["Twist Clamp Problem", "Twist Door problem", "Twist Problem"], 
+    //   "USS M/C": ["Terminal Change", "Welding Machine Problem", "Welding Problem"], 
+    //   "Taping M/C": ["Tapping problem", "Tapping Touch Screen Problem"], 
+    //   "Solder": ["Solder Problem"]
+    // };
 
     for (var i = 0; i < subcategories[category].length; i++) {
+
       var subcat = subcategories[category][i];
 
       var td = document.createElement("td");
@@ -69,7 +71,7 @@ function copyText(button) {
       button.innerHTML = subcat;
       button.onclick = function() {
         copyText(this);
-        showSubsubcategories(this);
+        showSubsubcategories(this.innerHTML);
       }
       button.addEventListener("mouseout", function() {
         outFunc(this);
@@ -82,10 +84,14 @@ function copyText(button) {
   }
 
   function showSubsubcategories(subcategory) {
-    var title = document.getElementById("subsubcat-title");
-    var buttons = document.getElementById("subsubcat-buttons");
-    title.innerHTML = "Faults For " + subcategory;
-    buttons.innerHTML = "";
+
+    document.querySelector('.sub-sub-category').style.display = 'block';
+    var subSubCatTitle = document.getElementById("subsubcat-title");
+    var row = document.querySelector(".sub-sub-category-row");
+
+    subSubCatTitle.innerHTML = "Faults For " + subcategory;
+    row.innerHTML = "";
+
     var subsubcategories = {
       "Applicator Problem": ["Scan Applicator ID"], 
       "Schleuniger": ["Machine Problem"], 
@@ -109,14 +115,33 @@ function copyText(button) {
       "Taping M/C": ["Tapping problem", "Tapping Touch Screen Problem"], 
       "Solder": ["Solder Problem"]
     };
+
     for (var i = 0; i < subsubcategories[subcategory].length; i++) {
       var subsubcat = subsubcategories[subcategory][i];
+
+      var td = document.createElement("td");
+      td.classList.add("tooltip");
+
       var button = document.createElement("button");
-      button.className = "button";
+
+
+      var span = document.createElement("span");
+      span.innerHTML = "Click to Copy"
+      span.classList.add("tooltiptext");
+
       button.innerHTML = subsubcat;
       button.onclick = function() {
-        copyText(this.innerHTML);
-      };
-      buttons.appendChild(button);
+        copyText(this);
+      }
+
+      button.addEventListener("mouseout", function() {
+        outFunc(this);
+      });
+
+    
+      
+      td.appendChild(button);
+      td.appendChild(span);
+      row.appendChild(td);
     }
   }
--- static/to-do.md
+++ static/to-do.md
@@ -1,6 +1,9 @@
 # Global
 -   Whenever a user clicks on something just open the category and close and dissappear everything else
 
+# html
+-   Copy all categories from the main row
+
 # javascript
 -   Find a better way to store sub-cat dict and sub-sub-cat dict
 -   use "var" or maybe there is somthing better
